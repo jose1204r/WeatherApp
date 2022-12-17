9d90cc4971f1a9324d13de92d538929191f20ae4
jose <jrodriguez@itsolutionspcllc.com>
2022-12-17 14:47:10 -0500

    Weatherapp

---
+++ Weatherapp/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Net.Http;
+using Newtonsoft.Json.Linq;
+
+namespace Weatherapp
+    
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            do
+            {
+
+            
+            var client = new HttpClient();
+
+            Console.WriteLine("*************Wheather APP***************");
+            
+            Console.WriteLine("PLEASE ENTER CITY TO SEE CURRENT WEATHER :");
+                var cityname = Console.ReadLine().ToUpper(); ;
+                Console.Clear();
+                Console.WriteLine("Lodding Data");
+                for (int i = 0; i < 7; i++)
+                {   
+                   
+                    Console.Write(".");
+                    Thread.Sleep(1000);
+
+                }
+                Console.Clear();
+
+                var apiurl = $"http://api.weatherapi.com/v1/current.json?key=3f1fca3a394e404e9fd175827221712&q={cityname}";
+
+            var response = client.GetStringAsync(apiurl).Result;
+
+            var wheatherqoute = JObject.Parse(response).GetValue("current").ToString();
+            Console.WriteLine($"City :{cityname}  {wheatherqoute}");
+                Console.WriteLine("DO YOU LIKE TO ENTER NETOHER CITY YES/NO");
+                var usersleections = Console.ReadLine();
+
+                if (usersleections.ToLower() == "no") 
+                {
+                    Console.Clear();
+                  Environment.Exit(0);
+                
+                }
+                Console.Clear();
+
+               
+            } while (true);
+            
+        }
+    }
+}
